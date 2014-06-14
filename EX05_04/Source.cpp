928a7d91ba1cf8044fc1fcf1ce4011f8a9c2873e
bbream16 <bbream16@my.whitworth.edu>
2014-06-13 20:23:01 -0700

    EX05_04

---
+++ EX05_04/Source.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <ctime>
+using namespace std;
+
+template<typename T>
+void shuffle(vector<T>& v)
+{
+	int x = 0;
+	vector<int> temp(10);
+
+	for (int i = 0; i < 10; i++)
+	{
+		random_shuffle(v.begin(), v.end());
+	}
+	for (int i = 0; i < 10; i++)
+			cout << "Element " << x+i << " = " << v[i] << endl;
+}
+
+
+
+
+
+int main() {
+	int x=0;
+	vector<int> intVector(10);
+
+	cout << "Please enter ten integers: ";
+	for (int i = 0; i < 10; i++)
+	{
+		cin >> x;
+		intVector.push_back(x);
+	}
+	shuffle(intVector);
+
+	return 0;
+}
